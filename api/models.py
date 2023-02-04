b5772f35ae85d5d134d7f60f33a15f20d7c4a66a
kamilanindita <kamilanindita@gmail.com>
2023-02-04 22:31:31 +0700

    add product model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,21 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+import uuid
 
-# Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(
+        primary_key = True, 
+        default = uuid.uuid4,
+        editable = False, 
+        unique=True
+    )
+
+    name = models.TextField()
+    price = models.FloatField()
+    currency_code = models.CharField(max_length=5)
+    image_url = models.TextField()
+    url_site = models.TextField()
+    marketplace = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+
+    def __str__(self):
+        return "{}".format(self.id)
