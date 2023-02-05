5e19c2a5a81818c0e5b2cb9dadaeb31ebf92bbee
kamilanindita <kamilanindita@gmail.com>
2023-02-05 17:44:26 +0700

    add validator save scrap data

--- api/serializers.py
+++ api/serializers.py
@@ -9,6 +9,13 @@ class RequestScrapProductSerializer(serializers.ModelSerializer):
         fields = ["url"]
 
 class SaveScrapProductSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(allow_blank=False)
+    price = serializers.FloatField()
+    currency_code = serializers.CharField(allow_blank=False)
+    image_url = serializers.CharField(allow_blank=False)
+    url_site = serializers.CharField(allow_blank=False)
+    marketplace = serializers.CharField(allow_blank=False)
+
     class Meta:
         model = Product
         fields = ["name", "price", "currency_code", "image_url", "url_site", "marketplace"]
