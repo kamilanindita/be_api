2cb11bd83aac974f2ecb4e7d152c21e11e606f07
kamilanindita <kamilanindita@gmail.com>
2023-02-05 11:19:08 +0700

    add url total group by router

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from django.urls import path, include
 from .views import (
     ScrapApiView,
+    ScrappedTotalProductGrupBy
 )
 
 urlpatterns = [
     path('scrap', ScrapApiView.as_view()),
+    path('scrapped/total', ScrappedTotalProductGrupBy.as_view())
 ]
