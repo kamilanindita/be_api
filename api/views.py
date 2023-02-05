9fc2e56970576b23482d42da02c0d647349d5e92
kamilanindita <kamilanindita@gmail.com>
2023-02-05 11:18:32 +0700

    add total group by endpoint

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .scraper.scraper import Scraper
-from .serializers import RequestScrapProductSerializer, SaveScrapProductSerializer, ScrapProductResponseSerializer
+from .serializers import RequestScrapProductSerializer, SaveScrapProductSerializer, ScrapProductResponseSerializer, TotalScrapProductSerializer
 
 class ScrapApiView(APIView):
     # Scrap endpoint
@@ -37,3 +37,51 @@ class ScrapApiView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(validator.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ScrappedTotalProductGrupBy(APIView):
+    def get_group_by(self, params):
+        # group by default without date system and only by marketplace
+        query_group = "SELECT id, marketplace, COUNT(id) as total FROM api_product GROUP by marketplace"
+
+        # group by date and marketplace
+        if(params['date'] != None and params['date_start'] == None and params['date_end'] == None and params['month'] == None):
+            query_group = "SELECT id, marketplace, COUNT(id) as total FROM api_product WHERE DATE(created_at) = '%s' GROUP by marketplace" % (params['date'])
+       
+        # group by range date and marketplace
+        elif (params['date_start'] != None and params['date_end'] != None and params['date'] == None and params['month'] == None):
+            query_group = "SELECT id, marketplace, COUNT(id) as total FROM api_product WHERE DATE(created_at) >= '%s' AND DATE(created_at)  <= '%s' GROUP by marketplace" % (params['date_start'], params['date_end'])
+        
+        # goup by month and marketplace
+        elif (params['month'] != None and abs(int(params['month'])) > 0 and abs(int(params['month'])) <= 12 and params['date'] == None and params['date_start'] == None and params['date_end'] == None):
+            month = '0'+str(abs(int(params['month']))) if abs(int(params['month'])) < 10 else str(abs(int(params['month'])))
+            
+            query_group = "SELECT id, marketplace, COUNT(id) as total FROM api_product WHERE strftime('%m',created_at) = " + " '%s' GROUP by marketplace" % (month)
+
+        try:
+            products = Product.objects.raw(query_group)
+            serializer = TotalScrapProductSerializer(products, many=True)
+
+            return serializer.data
+
+        except Product.DoesNotExist:
+
+            return False
+
+    # Scrapped group by endpoint
+    def get(self, request, *args, **kwargs):
+        # get query params
+        date = self.request.query_params.get('date')
+        date_start = self.request.query_params.get('from')
+        date_end = self.request.query_params.get('to')
+        month = self.request.query_params.get('month')
+
+        params = {
+            "date": date,
+            "date_start": date_start,
+            "date_end": date_end,
+            "month": month
+        }
+
+        results = self.get_group_by(params)
+
+        return Response(results, status=status.HTTP_200_OK)
