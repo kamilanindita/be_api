99d8bcf50e4d94a97748187488d6b9bbeedd723f
kamilanindita <kamilanindita@gmail.com>
2023-02-06 08:31:31 +0700

    add specific timeout request

--- api/scraper/scraper.py
+++ api/scraper/scraper.py
@@ -14,7 +14,7 @@ class Scraper:
             headers = ({"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"})
             
             # HTTP Request
-            webpage = requests.get(self.url, headers=headers, timeout=None)
+            webpage = requests.get(self.url, headers=headers, timeout=30)
 
             # Soup Object containing all data
             soup = BeautifulSoup(webpage.content, "lxml")
