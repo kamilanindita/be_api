f098f3ab3e089dfc4f1d69c9ee3bb1caf8979720
kamilanindita <kamilanindita@gmail.com>
2023-02-06 15:27:10 +0700

    change header

--- api/scraper/scraper.py
+++ api/scraper/scraper.py
@@ -10,10 +10,8 @@ class Scraper:
         # Extract marketplace from url
         marketplace = self.url.split(".")[1]
 
-        if(marketplace=="amazon" or marketplace=="ebay"):                                                                                                                                                         
-            headers = ({'User-Agent':
-	            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-	            'Accept-Language': 'en-US, en;q=0.5'})
+        if(marketplace=="amazon" or marketplace=="ebay"):
+            headers = ({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Accept-Language': 'en-US, en;q=0.5'})
             # HTTP Request
             webpage = requests.get(self.url, headers=headers, timeout=30)
 
