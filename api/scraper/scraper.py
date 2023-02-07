8081679ec5d946aecb92faebc5fa70ea3da26f79
kamilanindita <kamilanindita@gmail.com>
2023-02-07 09:55:51 +0700

    handle error messages when a rate limit occurs

--- api/scraper/scraper.py
+++ api/scraper/scraper.py
@@ -12,15 +12,23 @@ class Scraper:
 
         if(marketplace=="amazon" or marketplace=="ebay"):
             headers = ({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36','Accept-Language': 'en-US, en;q=0.5'})
+
+            # proxy = 'http://14.198.21.70:80'
+
             # HTTP Request
             webpage = requests.get(self.url, headers=headers, timeout=30)
 
-            # Soup Object containing all data
-            soup = BeautifulSoup(webpage.content, "lxml")
+            if(webpage.status_code == 200):
+                # Soup Object containing all data
+                soup = BeautifulSoup(webpage.content, "lxml")
 
-            # Parser
-            parse = Parser(soup, marketplace)
-            return parse.extract()
+                # Parser
+                parse = Parser(soup, marketplace)
+                return parse.extract()
+                
+            elif(webpage.status_code == 503):
+                print("rate limit")
+                return "rate limit"
             
         else:
             return "marketplace not available"
